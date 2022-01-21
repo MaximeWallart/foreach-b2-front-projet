ddfe0f145566aa9e41797514861e59803e3078f2
Maxime Wallart <wallart.maxime.pro@gmail.com>
2022-01-21 12:21:36 +0100

    css sorta fixed, aligned with the center

--- src/views/CompareMovies.vue
+++ src/views/CompareMovies.vue
@@ -1,14 +1,22 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col cols="4">
-        <v-card elevation="10" max-width="500" @click="saveClickedMovie(movie1.id, movie2.id)">
-          <v-img contain :src="movie1.image"/>
+    <v-row class="row">
+      <v-col cols="6" class="col">
+        <v-card
+          elevation="10"
+          max-width="350"
+          @click="saveClickedMovie(movie1.id, movie2.id)"
+        >
+          <v-img contain :src="movie1.image" />
         </v-card>
       </v-col>
-      <v-col cols="4" class="versus">VS</v-col>
-      <v-col cols="4">
-        <v-card elevation="10" max-width="500" @click="saveClickedMovie(movie2.id, movie1.id)">
+      <v-spacer />
+      <v-col cols="6" class="col">
+        <v-card
+          elevation="10"
+          max-width="350"
+          @click="saveClickedMovie(movie2.id, movie1.id)"
+        >
           <v-img contain :src="movie2.image" />
         </v-card>
       </v-col>
@@ -31,57 +39,69 @@ export default {
   },
   methods: {
     saveClickedMovie(winner, loser) {
-        // console.log('movies : ' + this.movies.length + '  |  nmbr : ' + this.nmbr.length);
-        if(this.winners.length < this.movies.length/2) {
-            this.winners.push(winner)
-            this.losers.push(loser)
-            console.log(this.winners.length);
-            if(this.winners.length < this.movies.length/2-1) {
-                this.getRandomMovie()
-            }
-        } else {
-            window.alert("T'as choisis parmis tout les films ghibli")
-            console.log(this.winners);
+      // console.log('movies : ' + this.movies.length + '  |  nmbr : ' + this.nmbr.length);
+      if (this.winners.length < this.movies.length / 2 - 1) {
+        this.winners.push(winner);
+        this.losers.push(loser);
+        console.log(this.winners.length);
+        if (this.winners.length < this.movies.length / 2 - 1) {
+          this.getRandomMovie();
         }
+      } else {
+        window.alert("T'as choisis parmis tout les films ghibli");
+        console.log(this.winners);
+      }
     },
     async setUp() {
       try {
-        this.movies = await getAllMovies()
-        this.winners.push(this.movies[0].id)
-        this.getRandomMovie()
+        this.movies = await getAllMovies();
+        this.winners.push(this.movies[0].id);
+        this.getRandomMovie();
       } catch (e) {
-        this.error = e
+        this.error = e;
       }
-    },  
+    },
     async getRandomMovie() {
-      var idx1 = Math.floor(Math.random() * this.movies.length)
-      while (this.winners.indexOf(this.movies[idx1].id) != -1 || this.losers.indexOf(this.movies[idx1].id) != -1) {
-          idx1 = Math.floor(Math.random() * this.movies.length)
+      var idx1 = Math.floor(Math.random() * this.movies.length);
+      while (
+        this.winners.indexOf(this.movies[idx1].id) != -1 ||
+        this.losers.indexOf(this.movies[idx1].id) != -1
+      ) {
+        idx1 = Math.floor(Math.random() * this.movies.length);
       }
-    //   console.log('winner : ' + this.winners.indexOf(this.movies[idx1].id));
-    //   console.log(this.winners);
-    //   console.log('loser : ' + this.losers.indexOf(this.movies[idx1].id));
-    //   console.log(this.losers);
-      this.movie1 = this.movies[idx1]
+      //   console.log('winner : ' + this.winners.indexOf(this.movies[idx1].id));
+      //   console.log(this.winners);
+      //   console.log('loser : ' + this.losers.indexOf(this.movies[idx1].id));
+      //   console.log(this.losers);
+      this.movie1 = this.movies[idx1];
 
-      var idx2 = Math.floor(Math.random() * this.movies.length)
-        // console.log(this.nmbr.indexOf(this.movies[idx2].id));
-      while (idx2 == idx1 || this.winners.indexOf(this.movies[idx2].id) != -1 || this.losers.indexOf(this.movies[idx2].id) != -1) {
-        idx2 = Math.floor(Math.random() * this.movies.length)
+      var idx2 = Math.floor(Math.random() * this.movies.length);
+      // console.log(this.nmbr.indexOf(this.movies[idx2].id));
+      while (
+        idx2 == idx1 ||
+        this.winners.indexOf(this.movies[idx2].id) != -1 ||
+        this.losers.indexOf(this.movies[idx2].id) != -1
+      ) {
+        idx2 = Math.floor(Math.random() * this.movies.length);
       }
-      this.movie2 = this.movies[idx2]
+      this.movie2 = this.movies[idx2];
     },
   },
   async mounted() {
-    return this.setUp()
+    return this.setUp();
   },
 };
 </script>
 <style>
-.versus {
-    vertical-align: middle;
-    text-align: center;
-    font-size: 50;
-    height: 500;
+.row {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.col {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  vertical-align: middle;
 }
 </style>
